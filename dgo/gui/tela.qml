f6a8367166b7e4b77542c83bc8750fa9b0e4c751
ReinaldoJunioDias <reinaldodiasabreu@gmail.com>
2022-08-28 19:34:27 -0300

    Add nenu file/open

--- dgo/gui/tela.qml
+++ dgo/gui/tela.qml
@@ -1,6 +1,36 @@
 import QtQuick
 import QtQuick.Controls
+import QtQuick.Controls.Material
 
 ApplicationWindow {
     visible: true
+    width:  800 //1366
+    height: 800 //768
+    title: qsTr('Display Graphic Objects')
+    font.pixelSize: 15
+    Material.theme: 'Dark'
+
+    menuBar: MenuBar {
+        Menu {
+            id: menu
+            title: qsTr("File")
+            MenuItem{
+                id: file
+                text: 'Open'
+                onClicked: {
+                    bridge.open_file()
+                }
+            }
+        }
+    }
+
+    Row{
+        id: row_1
+        spacing: 20
+        anchors{
+            horizontalCenter: parent.horizontalCenter
+            topMargin: 20
+        }
+    }
+    
 }
--- dgo/main.py
+++ dgo/main.py
@@ -1,20 +1,36 @@
 import os
 from pathlib import Path
-import sys
 
 from PySide6.QtQml import QQmlApplicationEngine
 from PySide6.QtGui import QGuiApplication
-from PySide6.QtCore import QUrl
+from PySide6.QtCore import QUrl, Slot, QObject
+
+class Bridge(QObject):
+
+    @Slot()
+    def open_file(self):
+        print("Open File...")
 
 
 def start():
     app = QGuiApplication()
     engine = QQmlApplicationEngine()
+    bridge = Bridge()
+    context = engine.rootContext()
+    context.setContextProperty('bridge', bridge)
 
     CURRENT_DIRECTORY = Path(__file__).resolve().parent
     filename = os.fspath(CURRENT_DIRECTORY / "gui" / "tela.qml")
     url = QUrl.fromLocalFile(filename)
 
-    engine.load(url)
+    try:
+        engine.load(url)
+    except error:
+        print(error)
+    
     print("Start Aplication!")
     app.exec()
+
+
+if __name__ == "__main__":
+    start()
