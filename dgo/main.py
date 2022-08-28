ed4fa6d47afa42aa4aeb51bb51dad5e7bfc13b4f
ReinaldoJunioDias <reinaldodiasabreu@gmail.com>
2022-08-28 17:47:14 -0300

    Add gui

--- dgo/main.py
+++ dgo/main.py
@@ -1,3 +1,20 @@
+import os
+from pathlib import Path
+import sys
+
+from PySide6.QtQml import QQmlApplicationEngine
+from PySide6.QtGui import QGuiApplication
+from PySide6.QtCore import QUrl
+
 
 def start():
+    app = QGuiApplication()
+    engine = QQmlApplicationEngine()
+
+    CURRENT_DIRECTORY = Path(__file__).resolve().parent
+    filename = os.fspath(CURRENT_DIRECTORY / "gui" / "tela.qml")
+    url = QUrl.fromLocalFile(filename)
+
+    engine.load(url)
     print("Start Aplication!")
+    app.exec()
